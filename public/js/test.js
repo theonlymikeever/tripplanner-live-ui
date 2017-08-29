6d8befa7b0c3876efa5abcef95d74c74ec050c93
Gabriela Medina <gmedina229@gmail.com>
2017-08-29 19:33:05 -0400

    add button

--- public/js/test.js
+++ public/js/test.js
@@ -1,11 +1,32 @@
 $(document).ready(function() {
-  console.log('im ready')
-  console.log('my hotels', hotels)
   $.each(hotels, function( index, hotel ) {
     var html = '<option value=' + hotel.id + '>' + hotel.name + '</options>'
-    console.log('my html', html)
-    $('#hotel-list').find('select').append(html)
-//    console.log(value.name );
+    $('#hotel-list').find('select').append(html);
   });
-//  $(#hotel-list).append()
-})
+  $.each(restaurants, function( index, restaurant ) {
+    var html = '<option value=' + restaurant.id + '>' + restaurant.name + '</options>'
+    $('#restaurants-list').find('select').append(html);
+  });
+  $.each(activities, function( index, activity ) {
+    var html = '<option value=' + activity.id + '>' + activity.name + '</options>'
+    $('#activities-list').find('select').append(html);
+  });
+
+  $('#hotel-list').find('button').on('click', function(){
+    var text = $('#hotel-list').find('select option:selected').text();
+    var html = `<li class='list-group-item'>${text}<button class='btn btn-warning btn-sm pull-right'>x</button> <br clear='both'/></li> `;
+    $('#day-hotel-list').append(html);
+  });
+
+  $('#restaurants-list').find('button').on('click', function(){
+    var text = $('#restaurants-list').find('select option:selected').text();
+    var html = `<li class='list-group-item'>${text}<button class='btn btn-warning btn-sm pull-right'>x</button> <br clear='both'/></li> `;
+    $('#day-restaurants-list').append(html);
+  });
+  $('#activities-list').find('button').on('click', function(){
+    var text = $('#activities-list').find('select option:selected').text();
+    var html = `<li class='list-group-item'>${text}<button class='btn btn-warning btn-sm pull-right'>x</button> <br clear='both'/></li> `;
+    $('#day-activities-list').append(html);
+  });
+
+});
